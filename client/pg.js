8d64d510f714a76081eb693f694bc4fe7c9fa790
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-06-05 19:23:02 +0200

    first commit

--- client/pg.js
+++ client/pg.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg');
 
-const client = new Pool({});
+const client = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 client.connect();
 
 module.exports = client;
